1130f7b8e1113b52231f4e0a6c48ba9bb55266f3
abhijitrajput11 <109435556+abhijitrajput11@users.noreply.github.com>
2023-07-23 12:11:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,27 +8,6 @@ const path = require('path');
 const staticpath = path.join(__dirname)
 const port = 3000
 
-// disable developer mode
-document.addEventListener('contextmenu',function(e){
-        e.preventDefault();
-      })
-      document.onkeydown = function (e) {
-        if (event.keyCode == 123) {
-          return false;
-        }
-        if (event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) {
-          return false;
-        }
-        if (event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) {
-          return false;
-        }
-        if (event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) {
-          return false;
-        }
-        if (event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) {
-          return false;
-        }
-      }
 
 
 app.set('view engine','ejs');
