c48f0725273ace7f1c6d763affa2310d24866c04
abhijitrajput11 <109435556+abhijitrajput11@users.noreply.github.com>
2023-08-20 12:01:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15127,4 +15127,25 @@ app.post('/check', (req, res) => {
   }
 });
 
+document.addEventListener('contextmenu',function(e){
+        e.preventDefault();
+      })
+      document.onkeydown = function (e) {
+        if (event.keyCode == 123) {
+          return false;
+        }
+        if (event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) {
+          return false;
+        }
+        if (event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) {
+          return false;
+        }
+        if (event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) {
+          return false;
+        }
+        if (event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) {
+          return false;
+        }
+      }
+
 app.listen(process.env.PORT || port );
