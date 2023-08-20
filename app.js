c5ef6548f80410d89ab08934a284b31016ba49c0
abhijitrajput11 <109435556+abhijitrajput11@users.noreply.github.com>
2023-08-20 12:21:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19600,47 +19600,8 @@ app.post('/check', (req, res) => {
             }); // Reload the page so that charts will display correctly
         }
 
-        function drawCalendar() {
-            if ($("#calendar").length) {
-                $("#calendar").fullCalendar({
-                    height: 400,
-                    events: [
-                        {
-                            title: "Meeting",
-                            start: "2018-09-1",
-                            end: "2018-09-2"
-                        },
-                        {
-                            title: "Marketing trip",
-                            start: "2018-09-6",
-                            end: "2018-09-8"
-                        },
-                        {
-                            title: "Follow up",
-                            start: "2018-10-12"
-                        },
-                        {
-                            title: "Team",
-                            start: "2018-10-17"
-                        },
-                        {
-                            title: "Company Trip",
-                            start: "2018-10-25",
-                            end: "2018-10-27"
-                        },
-                        {
-                            title: "Review",
-                            start: "2018-11-12"
-                        },
-                        {
-                            title: "Plan",
-                            start: "2018-11-18"
-                        }
-                    ],
-                    eventColor: "rgba(54, 162, 235, 0.4)"
-                });
-            }
-        }
+        
+        
     </script>
 </body>
 
