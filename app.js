0b088c5174c93e9d10a55893f8a76919e5ada4c1
abhijitrajput11 <109435556+abhijitrajput11@users.noreply.github.com>
2023-08-21 18:08:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19184,7 +19184,7 @@ app.post('/check', (req, res) => {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Marksheet | Abhijit</title>
     
